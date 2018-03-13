c3ab27e85196f518b6891f7aa83dc8efb30a0919
Unknown <felicie.godineau@gmail.com>
2018-03-13 12:15:39 -0700

    [TABS] Change names and icons

--- app/src/main/java/example/com/familyagenda/MainActivity.java
+++ app/src/main/java/example/com/familyagenda/MainActivity.java
@@ -93,27 +93,23 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera)
+        if (id == R.id.nav_calendar)
         {
             // Handle the camera action
         }
-        else if (id == R.id.nav_gallery)
+        else if (id == R.id.nav_groceries)
         {
 
         }
-        else if (id == R.id.nav_slideshow)
+        else if (id == R.id.nav_todo)
         {
 
         }
-        else if (id == R.id.nav_manage)
+        else if (id == R.id.nav_chores)
         {
 
         }
-        else if (id == R.id.nav_share)
-        {
-
-        }
-        else if (id == R.id.nav_send)
+        else if (id == R.id.nav_settings)
         {
 
         }
