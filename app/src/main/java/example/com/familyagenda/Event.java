46bbee6aa8b3613b1c2b8b979c54b1f47cbf0c12
Evan Pascual <pascual.evan@yahoo.com>
2018-04-10 13:31:46 -0700

    Added Variable
    Just added some new variables to the event class, and the getters/setters for them

--- app/src/main/java/example/com/familyagenda/Event.java
+++ app/src/main/java/example/com/familyagenda/Event.java
@@ -22,8 +22,18 @@ public class Event
     private int year;
     private int month;
     private int dayOfMonth;
+    /**
+     * Start time
+     * */
+    private int startTimeHour;
+    private int startTimeMinute;
+    /**
+     * End time
+     * */
+    private int endTimeHour;
+    private int endTimeMinute;
 
-    /** Members who are associated with the Event */
+    // Members who are associated with the Event
     //private Members[] members;
 
     /** Boolean for when the event is repeatable (e.g., birthdays, anniversaries) */
@@ -35,12 +45,25 @@ public class Event
      * @param title - Title of event
      * @param desc - Description of event
      * @param repeats - Event is repeatable
+     * @param startTimeHour - Hour(in military time) that the event starts
+     * @param startTimeMinute - Minute that the event starts
+     * @param endTimeHour - Hour(in military time) that the event ends
+     * @param endTimeMinute - Minute that the event ends
      */
-    public Event(String title, String desc, boolean repeats)
+    public Event(String title, String desc, int dayOfMonth, int month, int year,
+                 boolean repeats, int startTimeHour,
+                 int startTimeMinute,int endTimeHour, int endTimeMinute)
     {
         this.title = title;
         this.desc = desc;
         this.repeats = repeats;
+        this.dayOfMonth = dayOfMonth;
+        this.month = month;
+        this.year = year;
+        this.startTimeHour = startTimeHour;
+        this.startTimeMinute = startTimeMinute;
+        this.endTimeHour = endTimeHour;
+        this.endTimeMinute = endTimeMinute;
     }
 
     /**
@@ -51,11 +74,25 @@ public class Event
     public String getTitle() { return title; }
 
     /**
+     * Setter for the title of the event
+     *
+     * @param newTitle- new title name
+     * */
+    public void setTitle(String newTitle){title = newTitle;}
+
+    /**
      * Getter for Event description
      *
      * @return - Description for event
      */
     public String getDesc() { return desc; }
+
+    /**
+     * Setter for the description of the event
+     *
+     * @param newDesc- new description
+     * */
+    public void setDesc(String newDesc){desc = newDesc;}
 
     /**
      * Getter for Event's repeatable option
@@ -64,6 +101,128 @@ public class Event
      */
     public boolean isRepetitive() { return repeats; }
 
+    /**
+     * Changes whether or not the event is repetitive
+     * */
+    public void changeRepetitive(){
+        if(repeats){
+            repeats = false;
+        }
+        else{
+            repeats = true;
+        }
+}
+    /**
+     * Getter for the day of the month
+     *
+     * @return - the day of the month
+     * */
+    public int getDayOfMonth(){return dayOfMonth;}
+
+    /**
+     * Setter for the day of the month for the event
+     *
+     * @param newDay - sets the day of the month
+     * */
+    public void setDayOfMonth(int newDay){dayOfMonth = newDay;}
+
+    /**
+     * Getter for the month
+     *
+     * @return - the month
+     * */
+    public int getMonth(){return month;}
+
+    /**
+     * Setter for the month of the event
+     *
+     * @param newMonth- the new month (must be within 1 and 12)
+     * */
+    public void setMonth(int newMonth){
+        if(newMonth <= 12 && newMonth > 0){
+            month = newMonth;
+        }
+    }
+
+    /**
+     * Getter for the year of the event
+     *
+     * @return the year of the event
+     * */
+    public int getYear(){return year;}
+
+    /**
+     * Setter for the year of the event
+     *
+     * @param newYear- the new year of the event
+     * */
+    public void setYear(int newYear){
+        year = newYear;
+    }
+
+    /**
+     * Getter for Event start time hour
+     *
+     * @return - Hour of Start Time(in military time)
+     * */
+    public int getStartTimeHour(){return startTimeHour;}
+
+    /**
+     * Setter for the start time hour of the event
+     *
+     * @param newStartTimeHour- the new start time hour
+     * */
+    public void setStartTimeHour(int newStartTimeHour){
+        startTimeHour = newStartTimeHour;
+    }
+
+    /**
+     * Getter for Event start time minute
+     *
+     * @return startTimeMinute - Minute of Start Time
+     * */
+    public int getStartTimeMinute(){return startTimeMinute;}
+
+    /**
+     * Setter for the start time minute of the event
+     *
+     * @param newStartTimeMinute- the start time minute of the event
+     * */
+    public void setStartTimeMinute(int newStartTimeMinute) {
+        startTimeMinute = newStartTimeMinute;
+    }
+
+    /**
+     * Getter for Event end time hour
+     *
+     * @return - Hour of End Time(in military time)
+     * */
+    public int getEndTimeHour(){return endTimeHour;}
+
+    /**
+     * Setter for the end time hour of the event
+     *
+     * @param newEndTimeHour - the end time hour for the event
+     * */
+    public void setEndTimeHour(int newEndTimeHour) {
+        endTimeHour = newEndTimeHour;
+    }
+
+    /**
+     * Getter for Event end time minute
+     *
+     * @return - Minute of Start Time(in military time)
+     * */
+    public int getEndTimeMinute(){return endTimeMinute;}
+
+    /**
+     * Setter for the end time minute of the event
+     *
+     * @param newEndTimeMinute - new end time minute for the event
+     * */
+    public void setEndTimeMinute(int newEndTimeMinute) {
+        endTimeMinute = newEndTimeMinute;
+    }
 
 
 }
---
+++ app/src/main/java/example/com/familyagenda/EventList.java
@@ -0,0 +1,46 @@
+package example.com.familyagenda;
+
+/**
+ * Created by Evan on 4/5/18.
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EventList {
+    public static List<Event> eventList = new ArrayList<>();
+    public static Map<String, Event> eventMap = new HashMap<>();
+
+    static {
+//        addEvent("Do Laundry", "Evan", false);
+    }
+
+    private static void addEvent(String eventTitle,
+                                   String desc, boolean repeats, int startTimeHour, int startTimeMinute,
+                                    int endTimeHour, int endTimeMinute)
+
+    public static List<String> getProductNames() {
+        List<String> list = new ArrayList<>();
+        for (Event product : eventList) {
+            list.add(product.getTitle());
+        }
+        return list;
+    }
+
+    public static List<Event> getFilteredList(String searchString) {
+
+        List<Event> filteredList = new ArrayList<>();
+        for (Event product : eventList) {
+            if (product.getTitle().contains(searchString)) {
+                filteredList.add(product);
+            }
+        }
+
+        return filteredList;
+
+    }
+
+}
+
