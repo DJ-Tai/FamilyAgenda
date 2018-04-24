4031a50f5f70626567bb9147e94f780be24d826f
Unknown <felicie.godineau@gmail.com>
2018-04-24 11:59:58 -0700

    Groceries with db

--- app/src/main/java/example/com/familyagenda/Groceries.java
+++ app/src/main/java/example/com/familyagenda/Groceries.java
@@ -1,6 +1,8 @@
 package example.com.familyagenda;
 
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -28,11 +30,34 @@ public class Groceries extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_groceries, container, false);
         ListView lv = (ListView) view.findViewById(R.id.groceries_list);
+        //db stuff
+        FamilyAgendaDbHelper dbHelper = new FamilyAgendaDbHelper(getContext());
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] selectionArgs = {"FALSE"};
+        Cursor cursor = db.query(
+                FamilyAgendaContract.FamilyAgendaGroceries.TABLE_NAME,   // The table to query
+                null,             // The array of columns to return (pass null to get all)
+                FamilyAgendaContract.FamilyAgendaGroceries.COLUMN_NAME_IS_DONE + " = ?",              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                null               // The sort order
+        );
 
         List<String> groceries = new ArrayList<String>();
+        while(cursor.moveToNext()) {
+            String item = cursor.getString(
+                    cursor.getColumnIndexOrThrow(FamilyAgendaContract.FamilyAgendaGroceries.COLUMN_NAME_ITEM));
+            groceries.add(item);
+        }
+        cursor.close();
+
+        /*List<String> groceries = new ArrayList<String>();
+
         groceries.add("Nutella");
         groceries.add("Beer");
-        groceries.add("Guacamole");
+        groceries.add("Guacamole"); */
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, groceries);
 
