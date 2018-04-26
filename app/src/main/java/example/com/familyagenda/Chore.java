e9acd70c38732d899299f3507e0129edc9207887
alextran1357 <alextran1357@gmail.com>
2018-04-26 10:05:36 -0700

    Slight change
    Added one button

--- app/src/main/java/example/com/familyagenda/Chore.java
+++ app/src/main/java/example/com/familyagenda/Chore.java
@@ -7,19 +7,11 @@ package example.com.familyagenda;
 public class Chore {
     private String name;
 
-    private int year;
-    private int month;
-    private int dayOfMonth;
-
     private boolean complete = false;
-    private boolean repeat = false;
 
-    public Chore(String name, int year, int month, int dayOfMonth, boolean repeat) {
+
+    public Chore(String name) {
         this.name = name;
-        this.year = year;
-        this.month = month;
-        this.dayOfMonth = dayOfMonth;
-        this.repeat = repeat;
     }
 
     public String getName() {
