2b895f140ef5726d857ab86357347deafa40b20f
David T <david.jt@protonmail.com>
2018-04-30 17:48:11 -0700

    Modified Calendar Java and XML file
    Adjusted Calendar layout; added code to grab device date BUT it loads really slow. Will figure it out eventually.

--- app/src/main/java/example/com/familyagenda/Calendar.java
+++ app/src/main/java/example/com/familyagenda/Calendar.java
@@ -8,15 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Date;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -47,15 +43,23 @@ public class Calendar extends Fragment
         /* References the CalendarView in the UI */
         CalendarView calendarView;
 
+        // Set date currently showing above the Calendar Events list
+        // TODO: THIS WORKS BUT IT MAKES IT MAD SLOW. DEBUGGING.
+//        char[] deviceDate = getDate();
+//        TextView dateTv = view.findViewById(R.id.tv_date_sel);
+//        dateTv.setText(deviceDate, 0, deviceDate.length);
+
+
         // TODO: DEBUGGING CALENDAR VIEW
         calendarView = view.findViewById(R.id.calendar_view);
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
+        {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calView, int year, int month,
                                             int dayOfMonth)
             {
-                String date = month + "/" + dayOfMonth + "/" + year;
+                String date = (month + 1) + "/" + dayOfMonth + "/" + year;
                 char[] charDate = date.toCharArray();
 
                 View outerView = calView.getRootView();
@@ -66,15 +70,21 @@ public class Calendar extends Fragment
             }
         });
 
-        // TODO: DEBUGGINGs Construct data source
+        // TODO: BELOW -\/ connect via Database
         ArrayList<Event> events = new ArrayList<>();
         events.add(new Event("Birthday Party", "Description of Evan's Birthday Party",
-                                20, 4, 2018, true,
-                                10,00,18,00));
+                             20, 4, 2018, true,
+                             10, 00, 18, 00));
 
         events.add(new Event("Soccer Game", "Description of CSULB vs CSUF Soccer Game",
-                                22, 4, 2018, false,
-                                10,30,13,0));
+                             22, 4, 2018, false,
+                             10, 30, 13, 0));
+        events.add(new Event("Basketball Game", "Description of CSULB vs CSUF Basketball Game",
+                             10, 6, 2018, false,
+                             10, 30, 13, 30));
+        events.add(new Event("Coding Session", "Something about Coding and What Not and Coffee",
+                             4, 5, 2018, false,
+                             7, 30, 9, 0));
 
         // TODO: Add click-to-edit events
         // Grab list of events and display them
@@ -85,6 +95,83 @@ public class Calendar extends Fragment
         return view;
     }
 
-
+//    /**
+//     * Get the date as a char[] for setting the TextView
+//     *
+//     * @return - Date as a char[]
+//     */
+//    public char[] getDate()
+//    {
+//        java.util.Date deviceDate = java.util.Calendar.getInstance().getTime();
+//        String[] splitDate = deviceDate.toString().split(" ");
+//
+//        String month = splitDate[1];
+//        month = getMonthNum(month);
+//        String day = splitDate[2];
+//        if (day.charAt(0) == '0')
+//        {
+//            day = new String(day.substring(1,2));
+//        }
+//
+//        String year = splitDate[5];
+//        String convertedDate = new String(month + "/" + day + "/" + year);
+//
+//        char[] forTextView = convertedDate.toCharArray();
+//
+//        return forTextView;
+//    }
+//
+//    /**
+//     * Gets the month as a number
+//     *
+//     * @param month - Name of month
+//     * @return - Month number [01:12]
+//     */
+//    public String getMonthNum(String month)
+//    {
+//        String num;
+//
+//        switch (month)
+//        {
+//            case "January":
+//                num = "1";
+//                break;
+//            case "February":
+//                num = "2";
+//                break;
+//            case "March":
+//                num = "3";
+//                break;
+//            case "April":
+//                num = "4";
+//                break;
+//            case "May":
+//                num = "5";
+//                break;
+//            case "June":
+//                num = "6";
+//                break;
+//            case "July":
+//                num = "7";
+//                break;
+//            case "August":
+//                num = "8";
+//                break;
+//            case "September":
+//                num = "9";
+//                break;
+//            case "October":
+//                num = "10";
+//                break;
+//            case "November":
+//                num = "11";
+//                break;
+//            default:
+//                num = "12";
+//                break;
+//        }
+//
+//        return new String(num);
+//    }
 
 }
