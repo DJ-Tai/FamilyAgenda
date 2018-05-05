c69588405ada25d16623277b1de522f2b8d433fe
Unknown <felicie.godineau@gmail.com>
2018-05-04 23:19:37 -0700

    Groceries : Add Suggested items list

--- app/src/main/java/example/com/familyagenda/Groceries.java
+++ app/src/main/java/example/com/familyagenda/Groceries.java
@@ -19,8 +19,11 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.Console;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 /**
@@ -36,12 +39,14 @@ public class Groceries extends Fragment implements View.OnClickListener {
     public View view;
     public List<String> groceries;
     public ArrayAdapter<String> arrayAdapter;
+    public List<String> suggestions;
+    public ArrayAdapter<String> arrayAdapterSuggestions;
     FamilyAgendaDbHelper dbHelper;
 
-    private void fetchGroceriesList() {
+    private ArrayList<String> fetchGroceriesList(String isDone) {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        String[] selectionArgs = {"FALSE"};
+        String[] selectionArgs = {isDone};
         Cursor cursor = db.query(
                 FamilyAgendaContract.FamilyAgendaGroceries.TABLE_NAME,   // The table to query
                 null,             // The array of columns to return (pass null to get all)
@@ -51,15 +56,18 @@ public class Groceries extends Fragment implements View.OnClickListener {
                 null,                   // don't filter by row groups
                 null               // The sort order
         );
-        groceries = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<String>();
         while(cursor.moveToNext()) {
             String item = cursor.getString(
                     cursor.getColumnIndexOrThrow(FamilyAgendaContract.FamilyAgendaGroceries.COLUMN_NAME_ITEM));
-            groceries.add(item);
+            list.add(item);
         }
         cursor.close();
+        return list;
     }
 
+
+    //Todo: not add if already there
     private void addItemInDb(String itemText) {
         // Gets the data repository in write mode
         SQLiteDatabase db = dbHelper.getWritableDatabase();
@@ -73,6 +81,32 @@ public class Groceries extends Fragment implements View.OnClickListener {
         long newRowId = db.insert(FamilyAgendaContract.FamilyAgendaGroceries.TABLE_NAME, null, values);
     }
 
+    private void SetDone(String item, String value) {
+        // Gets the data repository in write mode
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // New value for one column
+        ContentValues values = new ContentValues();
+        values.put(FamilyAgendaContract.FamilyAgendaGroceries.COLUMN_NAME_IS_DONE, value);
+
+        // Which row to update, based on the title
+        String selection = FamilyAgendaContract.FamilyAgendaGroceries.COLUMN_NAME_ITEM + " LIKE ?";
+        String[] selectionArgs = { item };
+
+        db.update(FamilyAgendaContract.FamilyAgendaGroceries.TABLE_NAME, values, selection, selectionArgs);
+    }
+
+    private void getSuggestions()
+    {
+        suggestions = fetchGroceriesList("TRUE");
+        Set<String> tmp = new HashSet<>();
+        for (String item : suggestions)
+            if (groceries.indexOf(item) == -1)
+                tmp.add(item);
+        suggestions.clear();
+        suggestions.addAll(tmp);
+    }
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -80,9 +114,13 @@ public class Groceries extends Fragment implements View.OnClickListener {
         view = inflater.inflate(R.layout.fragment_groceries, container, false);
         dbHelper = new FamilyAgendaDbHelper(getContext());
         ListView lv = (ListView) view.findViewById(R.id.groceries_list);
-        fetchGroceriesList();
+        ListView lv_suggestions = (ListView) view.findViewById(R.id.groceries_suggestions);
+        groceries = fetchGroceriesList("FALSE");
+        getSuggestions();
         arrayAdapter = new GroceriesItemList(view.getContext(), R.layout.groceries_items_list, groceries);
+        arrayAdapterSuggestions = new GroceriesSuggestedItemsList(view.getContext(), R.layout.groceries_suggested_items_list, suggestions);
         lv.setAdapter(arrayAdapter);
+        lv_suggestions.setAdapter(arrayAdapterSuggestions);
 
         Button b = (Button) view.findViewById(R.id.btnAddItem);
         b.setOnClickListener(this);
@@ -90,14 +128,23 @@ public class Groceries extends Fragment implements View.OnClickListener {
         return view;
     }
 
+    private void addItem(String newItem) {
+        if (groceries.indexOf(newItem) != -1) //item already in list
+        {
+            Toast.makeText(getContext(), "Item already in the groceries list", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            arrayAdapter.add(newItem);
+            addItemInDb(newItem);
+        }
+    }
+
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
             case R.id.btnAddItem:
                 EditText etNewItem = (EditText) view.findViewById(R.id.etNewItem);
-                String itemText = etNewItem.getText().toString();
-                arrayAdapter.add(itemText);
-                addItemInDb(itemText);
+                addItem(etNewItem.getText().toString());
                 etNewItem.setText("");
                 break;
             default:
@@ -123,23 +170,62 @@ public class Groceries extends Fragment implements View.OnClickListener {
                 ViewHolder viewHolder = new ViewHolder();
                 viewHolder.checkBox = (CheckBox) convertView.findViewById(R.id.list_item_checkBox);
                 viewHolder.title = (TextView) convertView.findViewById(R.id.list_item_text);
-                viewHolder.button = (Button) convertView.findViewById(R.id.list_item_remove);
+                //viewHolder.button = (Button) convertView.findViewById(R.id.list_item_remove);
+                convertView.setTag(viewHolder);
+            }
+            mainViewholder = (ViewHolder) convertView.getTag();
+            /*mainViewholder.button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Button was clicked for list item " + position, Toast.LENGTH_SHORT).show();
+
+                }
+            }); */
+            final ViewHolder finalMainViewholder = mainViewholder;
+            mainViewholder.checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(getContext(), "CheckBox was clicked for list item " + position, Toast.LENGTH_SHORT).show();
+                    if (finalMainViewholder.checkBox.isChecked())
+                        SetDone(groceries.get(position), "TRUE");
+                    else
+                        SetDone(groceries.get(position), "FALSE");
+                }
+            });
+            mainViewholder.title.setText(getItem(position));
+
+            return convertView;
+        }
+    }
+
+    private class GroceriesSuggestedItemsList extends ArrayAdapter<String> {
+        private int layout;
+        private List<String> mObjects;
+        private GroceriesSuggestedItemsList(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            mObjects = objects;
+            layout = resource;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewholder = null;
+            if(convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.button = (Button) convertView.findViewById(R.id.list_suggested_item);
                 convertView.setTag(viewHolder);
             }
             mainViewholder = (ViewHolder) convertView.getTag();
             mainViewholder.button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(getContext(), "Button was clicked for list item " + position, Toast.LENGTH_SHORT).show();
+                    addItem(suggestions.get(position));
+                    arrayAdapterSuggestions.remove(suggestions.get(position));
                 }
             });
-            mainViewholder.checkBox.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(getContext(), "CheckBox was clicked for list item " + position, Toast.LENGTH_SHORT).show();
-                }
-            });
-            mainViewholder.title.setText(getItem(position));
+            mainViewholder.button.setText(getItem(position));
 
             return convertView;
         }
