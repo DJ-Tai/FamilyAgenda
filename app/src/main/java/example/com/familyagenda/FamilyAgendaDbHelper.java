692ab09e08e3e3afaf13185b8990114ded5f1dc8
Unknown <sambranocasey1@gmail.com>
2018-05-08 12:40:30 -0700

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/example/com/familyagenda/FamilyAgendaDbHelper.java
+++ app/src/main/java/example/com/familyagenda/FamilyAgendaDbHelper.java
@@ -1,19 +1,22 @@
 package example.com.familyagenda;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues;
 
-import example.com.familyagenda.database.EventsTable;
+import java.util.ArrayList;
 
 /**
  * Created by Helidia on 4/16/2018.
  */
 
-public class FamilyAgendaDbHelper extends SQLiteOpenHelper
-{
-    public static final String DB_NAME = "FamilyAgenda.db";
-    public static final int DB_VERSION = 1;
+public class FamilyAgendaDbHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "FamilyAgenda.db";
+    private static final int DB_VERSION = 1;
+    public static final String DB_Table = "Task";
+    public static final String DB_COLUM = "TaskName";
 
     public FamilyAgendaDbHelper(Context context)
     {
@@ -24,7 +27,7 @@ public class FamilyAgendaDbHelper extends SQLiteOpenHelper
     public void onCreate(SQLiteDatabase db)
     {
         db.execSQL(FamilyAgendaContract.SQL_CREATE_GROCERIES);
-        db.execSQL(EventsTable.SQL_CREATE_EVENTS);
+        String query = String.format("CREATE TABLE %s (ID INTEGER PRIMARY KEY AUTOINCREMENT,%s TEXT NOT NULL",DB_Table,DB_COLUM);
     }
 
     @Override
@@ -33,10 +36,38 @@ public class FamilyAgendaDbHelper extends SQLiteOpenHelper
         db.execSQL(FamilyAgendaContract.SQL_DELETE_GROCERIES);
         db.execSQL(EventsTable.SQL_DELETE_EVENTS);
         onCreate(db);
+        String query = String.format("DELETE TABLE IF EXISTS %s",DB_Table);
     }
 
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion)
     {
         onUpgrade(db, oldVersion, newVersion);
+    }
+    public void newTask(String task){
+        SQLiteDatabase x = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DB_COLUM,task);
+        x.insertWithOnConflict(DB_Table,null,values,SQLiteDatabase.CONFLICT_REPLACE);
+        x.close();
+    }
+    public void delete(String task)
+    {
+        SQLiteDatabase x = this.getWritableDatabase();
+        x.delete(DB_Table,DB_COLUM+" - ?",new String[]{task});
+        x.close();
+    }
+    public ArrayList<String> gettodolist()
+    {
+        ArrayList<String> list = new ArrayList<>();
+        SQLiteDatabase x = this.getReadableDatabase();
+        Cursor cur=x.query(DB_Table,new String[]{DB_COLUM},null,null,null,null,null);
+        while(cur.moveToNext())
+        {
+            int index = cur.getColumnIndex(DB_COLUM);
+            list.add(cur.getString(index));
+        }
+        cur.close();
+        x.close();
+        return list;
     }
 }
--- app/src/main/java/example/com/familyagenda/ToDo.java
+++ app/src/main/java/example/com/familyagenda/ToDo.java
@@ -4,8 +4,20 @@ package example.com.familyagenda;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.View;
+import android.view.*;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
 
 
 /**
@@ -13,6 +25,9 @@ import android.view.ViewGroup;
  */
 public class ToDo extends Fragment {
 
+    FamilyAgendaDbHelper db;
+    ArrayAdapter<String> som;
+    ListView tasks;
 
     public ToDo() {
         // Required empty public constructor
@@ -23,7 +38,59 @@ public class ToDo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        super.onCreate(savedInstanceState);
+        db = new FamilyAgendaDbHelper(this.getContext());
+        tasks = (ListView)container.findViewById(R.id.lstTask);
         return inflater.inflate(R.layout.fragment_to_do, container, false);
+    }
+
+    private void loadList()
+    {
+        ArrayList<String>list=db.gettodolist();
+        if(som==null)
+        {
+            som = new ArrayAdapter<String>(this.getContext(),R.layout.row,R.id.eventtitle,list);
+            tasks.setAdapter(som);
+        }
+        else
+        {
+            som.clear();
+            som.addAll(list);
+            som.notifyDataSetChanged();
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem x) {
+        switch (x.getItemId()){
+            case R.id.Add:
+                final EditText edit = new EditText(this.getContext());
+                AlertDialog dialog = new AlertDialog.Builder(this.getContext())
+                        .setTitle("Add to your to-do list.")
+                        .setMessage("Done")
+                        .setView(edit)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int y) {
+                                String task = String.valueOf(edit.getText());
+                                db.newTask(task);
+                                loadList();
+                            }
+                        })
+                        .setNegativeButton("Cancel",null)
+                        .create();
+                dialog.show();
+                return true;
+        }
+        return super.onOptionsItemSelected(x);
+    }
+    public void delete(View x)
+    {
+        View parent = (View)x.getParent();
+        TextView task = (TextView)parent.findViewById(R.id.eventtitle);
+        Log.e("String",(String) task.getText());
+        String event = String.valueOf(task.getText());
+        db.delete(event);
+        loadList();
     }
 
 }
